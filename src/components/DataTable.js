59b95da324739c5b89327c23194e75a50e93e1bf
Dustin Walker <dustinwalkerart@gmail.com>
2020-09-06 03:39:48 +0900

    adds material ui

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react"
 
+const testData = [
+  { key: "test name", values: ["value1", "value2"] },
+  { key: "test name2", values: ["value1", "value2"] },
+  { key: "test name2", values: ["value1", "value2"] },
+  { key: "test name2", values: ["value1", "value2"] },
+]
 class DataTable extends Component {
+  createRows = keys => {
+    if (keys) {
+      return keys.map(item => {
+        return <th>{item.key}</th>
+      })
+    }
+  }
+
   render() {
-    return <p>Testing Table</p>
+    return (
+      <table>
+        <tr>{this.createRows(testData)}</tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+      </table>
+    )
   }
 }
 

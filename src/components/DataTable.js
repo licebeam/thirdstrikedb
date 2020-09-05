7e6816016029b41ca4766979926f313a2f4435c3
Dustin Walker <dustinwalkerart@gmail.com>
2020-09-06 03:00:28 +0900

    fixes css

---
+++ src/components/DataTable.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+class DataTable extends Component {
+  render() {
+    return <p>Testing Table</p>
+  }
+}
+
+export default DataTable
--- src/components/layout.js
+++ src/components/layout.js
@@ -30,11 +30,15 @@ const Layout = ({ children }) => {
         style={{
           margin: `0 auto`,
           maxWidth: 960,
-          height: `100%`,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </div>
     </>
   )
